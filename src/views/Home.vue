03c672cff9659b7f310a7319fa06b200bb8c3947
leiwen <657106593@qq.com>
2021-01-25 23:05:35 +0800

    feat：更新轮播广告

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,8 @@
     <!-- 轮播图 -->
     <div class="block">
       <el-carousel height="460px">
-        <el-carousel-item v-for="item in carousel" :key="item.carousel_id">
-          <img style="height:460px;" :src="$target + item.imgPath" :alt="item.describes" />
+        <el-carousel-item v-for="item in carousel" :key="item.id">
+          <a target="_blank" :href="item.url"><img style="height:460px;" :src="item.pic" :alt="item.name" /></a>
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -270,14 +270,14 @@ export default {
   },
   created() {
     // 获取轮播图数据
-    this.$axios
-      .post("/api/resources/carousel", {})
-      .then(res => {
-        this.carousel = res.data.carousel;
-      })
-      .catch(err => {
-        return Promise.reject(err);
-      });
+    // this.$axios
+    //   .post("/api/resources/carousel", {})
+    //   .then(res => {
+    //     this.carousel = res.data.carousel;
+    //   })
+    //   .catch(err => {
+    //     return Promise.reject(err);
+    //   });
     // 获取各类商品数据
     this.getPromo("手机", "phoneList");
     this.getPromo("电视机", "miTvList");
@@ -327,7 +327,8 @@ export default {
       request.get('/home/content', params).then((res) => {
         const { code, data } = res
         if (code === 200) {
-          this.brandList = data.brandList
+          this.carousel = data.advertiseList  // 获取轮播图数据
+          this.brandList = data.brandList     // 获取品牌数据
           console.log('response brandList is:', this.brandList)
         }
       })
